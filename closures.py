a35b52be21095706b13e2801445386f73e67458f
ajaygbl <ajaygbl@gmail.com>
2024-04-03 20:25:53 +0530

    accessing inner func from outer func

--- closures.py
+++ closures.py
@@ -4,9 +4,11 @@ def outer_func():
     def inner_func():
         print(message)
 
-    return inner_func()
+    return inner_func
 
-outer_func()
-
+my_func = outer_func()
+print(my_func)          # output --> <function outer_func.<locals>.inner_func at 0x108539080>
+print(my_func.__name__) # output -->  inner_func
 # accessing message variable in inner_func which is defined in outer_func
-#message variable is called free variable
+# message variable is called free variable
+
