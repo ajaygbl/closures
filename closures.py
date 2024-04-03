83f5581f124e42da7f2238f43c4dd53538f4657b
ajaygbl <ajaygbl@gmail.com>
2024-04-03 20:40:13 +0530

    passing argumnent msg

--- closures.py
+++ closures.py
@@ -1,15 +1,20 @@
-def outer_func():
-    message ='Hi'
+def outer_func(msg):
+    message = msg
 
     def inner_func():
         print(message)
 
     return inner_func
 
-my_func = outer_func()
-print(my_func)          # output --> <function outer_func.<locals>.inner_func at 0x108539080>
-print(my_func.__name__) # output -->  inner_func
-print(my_func())        # output --> Hi
+hi_func = outer_func('Hi')
+Hello_func = outer_func('Hello')
+
+hi_func()
+Hello_func()
+
+#print(my_func)          # output --> <function outer_func.<locals>.inner_func at 0x108539080>
+#print(my_func.__name__) # output -->  inner_func
+#my_func()        # output --> Hi
 
 # accessing message variable in inner_func which is defined in outer_func
 # message variable is called free variable
